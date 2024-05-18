5d8bf7d3fec378506608e5e06d3ba2da60e1421d
Veronika <99502903+Veronika181@users.noreply.github.com>
2024-05-18 21:03:56 +0200

    commit

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const left = document.querySelector(".left")
-const right = document.querySelector(".right")
-const slider = document.querySelector(".slider")
+const right = document.querySelector(".right");;
+const slider = document.querySelector(".slider");
+const images = document.querySelector(".image");
+
+let slideNumber = 1;
+
 
 right.addEventListener("click", () => {
-    slider.style.transform = 'translateX(-800px)'
-})
+    slider.style.transform = 'translateX(-${slideNumber * 800}px)';
+    slideNumber++;
+});
+
